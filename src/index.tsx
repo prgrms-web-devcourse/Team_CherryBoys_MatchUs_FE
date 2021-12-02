eb1cd5bb879a59f8dc064a020519c9fe63a9ffc9
progwon <dhwhc0711@naver.com>
2021-12-02 14:07:05 +0900

    chore: Redux 세팅 업로드
    Redux, ReduxToolKit 세팅 업로드

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from '@/App';
 import '@/styles/index.scss';
+import { store } from './store/index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
+
   document.getElementById('root')
 );
