41b849583a02462b4d5435603f178f742d54825c
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-12-03 16:40:35 +0900

    feat: API Core(axios) 로직 추가

---
+++ src/api/core.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosInstance, AxiosRequestConfig, Method } from 'axios';
+import { HTTP_METHODS } from '@/consts';
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: 'https://api.helltabus.com',
+  timeout: 10000,
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // TODO: 배포환경에서 백엔드에서 토큰을 받는 로직에 대해서 논의 필요
+}
+
+if (process.env.NODE_ENV === 'development') {
+  axiosInstance.defaults.headers.common.Authorization = process.env.DUMMY_TOKEN ?? '';
+}
+
+const createApiMethod =
+  (_axiosInstance: AxiosInstance, methodType: Method) =>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (config: AxiosRequestConfig): Promise<any> => {
+    _axiosInstance.interceptors.response.use((response) => {
+      if (!response.data) return response;
+      return response.data.data;
+    });
+
+    return _axiosInstance({
+      ...config,
+      method: methodType,
+    });
+  };
+
+export default {
+  get: createApiMethod(axiosInstance, HTTP_METHODS.GET),
+  post: createApiMethod(axiosInstance, HTTP_METHODS.POST),
+  patch: createApiMethod(axiosInstance, HTTP_METHODS.PATCH),
+  put: createApiMethod(axiosInstance, HTTP_METHODS.PUT),
+  delete: createApiMethod(axiosInstance, HTTP_METHODS.DELETE),
+};
