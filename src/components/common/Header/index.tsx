f34d33a45721dbe740968a8980abfdee4664a473
Yongrok <94chl@naver.com>
2021-12-04 20:54:49 +0900

    docs: 헤더 css 수정

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -15,15 +15,10 @@ const Header = () => {
       <div>
         <button onClick={handleGoBack}><FontAwesomeIcon icon={faChevronLeft} /></button>
       </div>
-      <div>      
-        <h1>
-          <Link to={'/'}>MatchUs</Link>
-        </h1>
-      </div>
       <div className={classNames(styles.buttonBox)}>
-        <button>
+        <button className={classNames(styles.loginButton)}>
           {/* 로그인 상태 생기면 토글로 로그인/로그아웃 구분 */}
-          <Link to={'/login'}>login</Link>
+          <Link to={'/login'}>로그인</Link>
         </button>
         <button>
           <Link to={'/setting'}><FontAwesomeIcon icon={faCog} /></Link>
