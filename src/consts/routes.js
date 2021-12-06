564013a13db7e6a7ce3ab0e57dc2a883cf7ec3cc
progwon <dhwhc0711@naver.com>
2021-12-06 18:02:52 +0900

    feat: router const 추가
    router path 관련한 라우팅 상수 추가

---
+++ src/consts/routes.js
@@ -0,0 +1,39 @@
+// 메인
+export const HOME_PAGE = '/';
+
+// 용병
+export const MERCENARY_PAGE = '/mercenary';
+export const MERCENARY_DETAIL_PAGE = '/mercenary/:postId';
+export const MERCENARY_POST_PAGE = '/mercenary/post/new';
+export const MERCENARY_EDIT_PAGE = '/mercenary/edit/:postId';
+export const MERCENARY_CHAT_PAGE = '/mercenary/chat/:postId';
+export const MERCENARY_ACCEPT_PAGE = '/mercenary/accept/:postId';
+
+// 매칭
+export const MATCHING_PAGE = '/matching/';
+export const MATCHING_POST_PAGE = '/matching/post/new';
+export const MATCHING_DETAIL_PAGE = '/matching/post/:postId';
+export const MATCHING_EDIT_PAGE = '/matching/edit/:postId';
+
+// 팀
+export const TEAM_PAGE = '/team/:teamId';
+export const TEAM_CREATE_PAGE = '/team/new';
+export const TEAM_MATCHING_LIST_PAGE = '/team/:teamId/match';
+export const TEAM_EDIT_PAGE = '/team/:teamId/edit';
+export const TEAM_SELECT_PAGE = '/team/select';
+export const TEAM_MEMBERS_PAGE = '/team/:teamId/member';
+export const TEAM_MEMBERS_EDIT_PAGE = '/team/:teamId/member/edit';
+
+// 회원
+export const SIGNUP_PAGE = '/signup';
+export const LOGIN_PAGE = '/login';
+
+// 개인
+export const USER_PAGE = '/user';
+export const USER_EDIT_PAGE = '/user/edit';
+export const USER_MATCHING_LIST_PAGE = '/user/match';
+export const USER_TEAM_INVITAION_LIST_PAGE = '/user/team/invitation';
+export const USER_MERCENARY_INVITAION_LIST_PAGE = '/user/mercenary/invitaion';
+
+// 예외처리
+export const NOT_FOUND_PAGE = '/not-found';
