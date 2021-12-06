bd758aba874ed051ca07eece6fabfbdfe1cd2c1c
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-12-06 17:51:58 +0900

    feat: CustomButton 구현

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,1 +1,3 @@
 export { default as CustomInput } from './CustomInput/CustomInput';
+export { default as CustomLabel } from './CustomLabel/CustomLabel';
+export { default as CustomButton } from './CustomButton/CustomButton';
