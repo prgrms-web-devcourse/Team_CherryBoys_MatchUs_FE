aa15a6e1e48ab73f46e374f93db4ec703b56951b
Yongrok <49222172+94chl@users.noreply.github.com>
2021-12-06 23:10:39 +0900

    feat: header, tabBar 컴포넌트 추가(MAT-42)
    * feat: header, tabBar 컴포넌트 추가
    
    * docs: 헤더 css 수정
    
    * docs: lint 적용
    
    * chore: fontawesome 관련 패키지 제거
    
    * refactor: scss 클래스명 구조분해할당으로 변경
    
    * chore: 각 컴포넌트의 index  파일명 변경(코드컨벤션)

---
+++ src/components/DefaultTemplate/DefaultTemplate.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import classNames from 'classnames';
+import { Header, TabBar } from '@/components/common';
+import styles from './DefaultTemplate.module.scss';
+
+interface Props {
+  children: ReactNode;
+}
+
+const DefaultTemplate = ({ children }: Props) => {
+  const { main } = styles;
+
+  return (
+    <BrowserRouter>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+      />
+      <Header />
+      <main className={classNames(main)}>{children}</main>
+      <TabBar />
+    </BrowserRouter>
+  );
+};
+
+export default DefaultTemplate;
