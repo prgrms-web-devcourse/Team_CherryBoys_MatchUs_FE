2a60eb7a793a15a0b9f05cd86dd28fa7a7b969a0
Yongrok <49222172+94chl@users.noreply.github.com>
2021-12-06 23:10:39 +0900

    feat: header, tabBar 컴포넌트 추가(MAT-42)
    * feat: header, tabBar 컴포넌트 추가
    
    * docs: 헤더 css 수정
    
    * docs: lint 적용
    
    * chore: fontawesome 관련 패키지 제거
    
    * refactor: scss 클래스명 구조분해할당으로 변경
    
    * chore: 각 컴포넌트의 index  파일명 변경(코드컨벤션)

---
+++ src/components/common/index.ts
@@ -0,0 +1,2 @@
+export { default as Header } from './Header/Header';
+export { default as TabBar } from './TabBar/TabBar';
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Matches } from './Matches/Matches';
+export { default as Hires } from './Hires/Hires';
+export { default as Teams } from './Teams/Teams';
+export { default as Users } from './Users/Users';
+export { default as Login } from './Login/Login';
+export { default as Setting } from './Setting/Setting';
