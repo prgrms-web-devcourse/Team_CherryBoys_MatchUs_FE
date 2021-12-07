7b2f6d20b668baf49042df5b6a8505ffbcd239b0
YRKlaptop <94chl@naver.com>
2021-12-07 17:24:21 +0900

    refactor: Match페이지 상대팀 존재 시 상대팀 정보 표시 적용

--- src/pages/Match/Match.tsx
+++ src/pages/Match/Match.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/jsx-fragments */
 import React, { Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import classNames from 'classnames';
 import { RootState } from '@/store';
 import { fetchMatchById } from '@/store/matches/matches';
 import { MatchInfo, TeamCard, MatchDetail, MatchButton } from '@/components/Match';
 import useMount from '@/hooks/useMount';
+import styles from './Match.module.scss';
+
+const { awayTeam, versus } = styles;
 
 const Match = () => {
   const dispatch = useDispatch();
@@ -21,7 +25,15 @@ const Match = () => {
         <Fragment key={`match${matchInfo.matchId}`}>
           <MatchInfo key={`matchInfo${matchInfo.matchId}`} match={matchInfo} />
           <TeamCard key={`teamInfo${matchInfo.homeTeam}`} team={matchInfo.homeTeam} />
-          <MatchDetail key={`matchDetail${matchInfo.matchId}`} match={matchInfo} />
+          {!matchInfo.awayTeam && (
+            <MatchDetail key={`matchDetail${matchInfo.matchId}`} match={matchInfo} />
+          )}
+          {matchInfo.awayTeam && (
+            <div className={classNames(awayTeam)}>
+              <div className={classNames(versus)}>VS</div>
+              <TeamCard key={`teamInfo${matchInfo.awayTeam}`} team={matchInfo.awayTeam} />
+            </div>
+          )}
         </Fragment>
       ))}
       <MatchButton enable={{ apply: true, approve: true }} />
