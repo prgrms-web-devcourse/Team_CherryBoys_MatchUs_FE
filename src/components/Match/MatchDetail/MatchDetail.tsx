d62b226fb30ae475dbfb3514d9de2516d83b7e48
YRKlaptop <94chl@naver.com>
2021-12-07 16:25:18 +0900

    feat: matchDetail컴포넌트(매치상세정보) 추가

---
+++ src/components/Match/MatchDetail/MatchDetail.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Link, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import styles from './MatchDetail.module.scss';
+import { Match } from '@/dummyMatch';
+import { deleteMatchById } from '@/store/matches/matches';
+
+interface Props {
+  match: Match;
+}
+
+const {
+  matchDetail,
+  matchDetail_menu,
+  menuName,
+  buttonBox,
+  editButton,
+  removeButton,
+  matchDetail_content,
+} = styles;
+
+const MatchDetail = ({ match }: Props) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleRemoveMatch = () => {
+    const matchId = parseInt(window.location.pathname.split('/')[2], 10);
+    if (window.confirm(`remove match${matchId}?`)) {
+      dispatch(deleteMatchById(matchId));
+      console.log(`${matchId} is REMOVED!`);
+      history.push('/matching/');
+    }
+  };
+
+  return (
+    <div className={classNames(matchDetail)}>
+      <div className={classNames(matchDetail_menu)}>
+        <h3 className={classNames(menuName)}>상세정보</h3>
+        <div className={classNames(buttonBox)}>
+          <button className={classNames(editButton)} type="button">
+            <Link to={`/matching/edit/${match.matchId}`}>
+              <i className="fas fa-pen" />
+            </Link>
+          </button>
+          <button className={classNames(removeButton)} type="button" onClick={handleRemoveMatch}>
+            <i className="fas fa-times" />
+          </button>
+        </div>
+      </div>
+      <div className={classNames(matchDetail_content)}>{match.detail}</div>
+    </div>
+  );
+};
+
+export default MatchDetail;
