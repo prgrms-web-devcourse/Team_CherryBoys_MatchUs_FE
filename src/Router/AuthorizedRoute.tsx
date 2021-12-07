937520aad58e741797f31fa150bef3ff88f58c82
progwon <dhwhc0711@naver.com>
2021-12-07 15:43:26 +0900

    feat: 구조분해할당 추가 및 명확한 props 작성
    구조분해할당 추가 및 exact, path props 추가했습니다

--- src/Router/AuthorizedRoute.tsx
+++ src/Router/AuthorizedRoute.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Redirect, Route, RouteProps } from 'react-router';
+import { Redirect, Route, RouteProps } from 'react-router-dom';
 import { getItemFromStorage } from '@/utils/storage';
 
 export interface AuthorizedRouterProps extends RouteProps {
@@ -8,13 +8,17 @@ export interface AuthorizedRouterProps extends RouteProps {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-shadow
-const AuthorizedRouter: React.FC<AuthorizedRouterProps> = (props) => {
+const AuthorizedRouter = ({
+  path,
+  component,
+  redirectPath = '/login',
+  exact,
+}: AuthorizedRouterProps) => {
   const token = getItemFromStorage('token') || null;
-  const { component, redirectPath } = props;
 
   return token ? (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <Route {...props} component={component} />
+    <Route exact={exact} component={component} path={path} />
   ) : (
     <Redirect to={{ pathname: redirectPath }} />
   );
