c1cfc826bc191df195bb4a7559cf1715855095e3
DongHyeon Kim <49136186+Kimbangg@users.noreply.github.com>
2021-12-07 16:01:37 +0900

    feat: team-sub-page 구현(MAT-51)
    * feat: CustomInput 구현
    
    * feat: CustomLabel 구현
    
    * feat: CustomButton 구현
    
    * feat: 팀 생성 페이지 마크업 구현
    
    * refactor: 불필요한 console.log 제거
    
    * feat: TeamInfoEdit 페이지 마크업
    
    * refactor: common 파일 import 방식 변경
    
    * refactor: AGE_GROUP & SPORT_CATEGORY 상수화
    
    * refactor: 불필요한 TeamCreate 컴포넌트 제거
    
    * refactor: 불필요한 TeamCreate 컴포넌트 제거
    
    * refactor: 불필요한 onClick 메소드 제거
    
    * chore: 추적되지 않은 파일 업로드
    
    * refactor: useFor범용적으로 사용할  타입 추가
    
    * feat: 팀 생성 및 수정 페이지에 사용되는 validation util 추가
    
    * feat: 팀 생성 및 수정 페이지에 validation 추가
    
    * style: 가독성 향상을 위한 개행 추가

---
+++ src/utils/validation/validation.ts
@@ -0,0 +1,27 @@
+// 팀 이름 REGEX
+export const TEAM_REGEX = {
+  IS_VALID_NAME: /^[가-힣\s|ㄱ-ㅎ|a-z|A-Z|0-9]+$/g,
+  HAS_SPACE_BAR: /\s/g,
+  MIN_LEN: 1,
+  MAX_LEN: 10,
+  BIO_MAX_LEN: 200,
+};
+
+export const TEAM_VALID_ERROR_MSG = {
+  IS_VALID_NAME: '팀 명은 한글, 영어, 숫자만 가능합니다',
+  IS_VALID_LEN: '팀 명은 1자 이상 10자 이하로 작성해주세요.',
+  HAS_SPACE: '팀 명에는 공백이 들어갈 수 없습니다.',
+  IS_VALID_BIO_LEN: '세부 설명은 200자 이하로 작성해야합니다.',
+  IS_TEAM_SPORT: '팀 종목을 선택해 주세요.',
+  IS_TEAM_AGE_GROUP: '팀 연령대를 선택해 주세요.',
+};
+
+export const validateTeamName = (teamName: string) => TEAM_REGEX.IS_VALID_NAME.test(teamName);
+
+export const validateTeamNameHasSpace = (teamName: string) =>
+  TEAM_REGEX.HAS_SPACE_BAR.test(teamName);
+
+export const validateTeamNameLength = (teamName: string) =>
+  TEAM_REGEX.MIN_LEN <= teamName.length && teamName.length <= TEAM_REGEX.MAX_LEN;
+
+export const validateTeamBioLength = (teamBio: string) => teamBio.length < TEAM_REGEX.BIO_MAX_LEN;
