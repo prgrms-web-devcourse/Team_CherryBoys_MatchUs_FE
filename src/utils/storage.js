4f26d9dcdb1caae617ba825c82e0fd642dc62c8b
progwon <dhwhc0711@naver.com>
2021-12-08 14:07:03 +0900

    refactor: localStorage 수정
    try-catch 삭제 후 TODO로 변경
    에러처리 구현 TODO로 변경

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,13 +1,7 @@
 const STORAGE = window.localStorage;
 
-const getItemFromStorage = (key, defaultValue) => {
-  try {
-    return JSON.parse(STORAGE.getItem(key));
-  } catch (e) {
-    STORAGE.removeItem(key);
-    // 에러 처리하기
-    return defaultValue;
-  }
+const getItemFromStorage = (key) => {
+  return JSON.parse(STORAGE.getItem(key));
 };
 
 const setItemFromStorage = (key, item) => {
