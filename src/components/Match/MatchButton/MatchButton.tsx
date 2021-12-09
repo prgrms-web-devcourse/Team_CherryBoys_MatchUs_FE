037c4e8979cb833d93cc522249dcaaaece8de337
Yongrok <94chl@naver.com>
2021-12-10 01:08:54 +0900

    refactor: 매칭 신청, 수락, 평가 모달 제어를 위한 버튼 기능 추가

--- src/components/Match/MatchButton/MatchButton.tsx
+++ src/components/Match/MatchButton/MatchButton.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import classNames from 'classnames';
+import { useDispatch } from 'react-redux';
 import styles from './MatchButton.module.scss';
+import { match } from '@/store/match/match';
 
 interface Props {
-  enable: { apply: boolean; approve: boolean };
+  enable: { apply: boolean; approve: boolean; review: boolean };
 }
 
 const { matchButtonBox, matchButton, approveButton, applyButton, show } = styles;
 
 // 사용자 정보랑 게시글 정보랑 비교해서 지원,수락여부 확인 필요
 const MatchButton = ({ enable }: Props) => {
+  const dispatch = useDispatch();
+
+  const onToggle = (modal: any) => {
+    dispatch(match.actions.toggleModal({ modalName: modal }));
+  };
+
   return (
     <div className={classNames(matchButtonBox)}>
       <button
@@ -17,6 +25,7 @@ const MatchButton = ({ enable }: Props) => {
           [show]: enable.approve,
         })}
         type="button"
+        onClick={() => onToggle('matchApprove')}
       >
         매칭 수락
       </button>
@@ -25,8 +34,18 @@ const MatchButton = ({ enable }: Props) => {
           [show]: enable.apply,
         })}
         type="button"
+        onClick={() => onToggle('matchApply')}
       >
         매칭 신청
+      </button>
+      <button
+        className={classNames(matchButton, applyButton, {
+          [show]: enable.apply,
+        })}
+        type="button"
+        onClick={() => onToggle('matchReview')}
+      >
+        매칭 평가
       </button>
     </div>
   );
