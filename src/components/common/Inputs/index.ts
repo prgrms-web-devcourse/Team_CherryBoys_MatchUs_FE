70fc773d25b6c9b459825bfff274c2c2308519ec
Yongrok <94chl@naver.com>
2021-12-10 01:14:53 +0900

    refactor: inputDetail 미완성 및 낮은 활용도로 삭제

--- src/components/common/Inputs/index.ts
+++ src/components/common/Inputs/index.ts
@@ -1,3 +1,2 @@
 export { default as Input } from './Input/Input';
 export { default as InputCheckBox } from './InputCheckBox/InputCheckBox';
-export { default as InputDetail } from './InputDetail/InputDetail';
