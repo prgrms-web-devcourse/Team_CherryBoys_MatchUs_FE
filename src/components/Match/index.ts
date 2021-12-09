9aa439c11f4d0b776f7a79346aae0ad2fc783cc2
Yongrok <94chl@naver.com>
2021-12-10 01:00:27 +0900

    docs: 미사용 컴포넌트 제거

--- src/components/Match/index.ts
+++ src/components/Match/index.ts
@@ -1,6 +1,4 @@
-export { default as MatchCard } from './MatchCard/MatchCard';
 export { default as MatchDetail } from './MatchDetail/MatchDetail';
 export { default as MatchInfo } from './MatchInfo/MatchInfo';
-export { default as MatchList } from './MatchList/MatchList';
 export { default as MatchButton } from './MatchButton/MatchButton';
 export { default as TeamCard } from './TeamCard/TeamCard';
