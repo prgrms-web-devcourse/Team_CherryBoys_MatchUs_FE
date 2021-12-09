cc182b5b7f16c81fde873aca406664bece09f00e
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-12-09 17:59:39 +0900

    refactor: 하드 코딩 -> List 컴포넌트를 이용해 렌더하는 방식으로 변경

--- src/pages/TeamPlayerManage/TeamPlayerManage.tsx
+++ src/pages/TeamPlayerManage/TeamPlayerManage.tsx
@@ -1,103 +1,8 @@
 import classNames from 'classnames';
 import React, { useState, useEffect } from 'react';
-import { Header } from '@/components';
+import { Header, PeopleList } from '@/components';
 import style from './teamPlayerManage.module.scss';
-
-// TODO: API 연결 이후에 삭제 예정( 2021.12.09 AM 01:51 )
-const dummyData = {
-  member: [
-    {
-      userId: 1,
-      userName: '김1',
-      grade: '회원',
-    },
-    {
-      userId: 2,
-      userName: '김2',
-      grade: '용병',
-    },
-    {
-      userId: 3,
-      userName: '오3',
-      grade: '용병',
-    },
-    {
-      userId: 4,
-      userName: '김4',
-      grade: '부주장',
-    },
-    {
-      userId: 5,
-      userName: '김5',
-      grade: '회원',
-    },
-    {
-      userId: 6,
-      userName: '오6',
-      grade: '회원',
-    },
-    {
-      userId: 7,
-      userName: '김7',
-      grade: '회원',
-    },
-    {
-      userId: 8,
-      userName: '김8',
-      grade: '회원',
-    },
-    {
-      userId: 9,
-      userName: '오9',
-      grade: '회원',
-    },
-    {
-      userId: 10,
-      userName: '김10',
-      grade: '주장',
-    },
-    {
-      userId: 11,
-      userName: '김11',
-      grade: '회원',
-    },
-    {
-      userId: 12,
-      userName: '오12',
-      grade: '회원',
-    },
-    {
-      userId: 13,
-      userName: '김13',
-      grade: '회원',
-    },
-    {
-      userId: 14,
-      userName: '오14',
-      grade: '회원',
-    },
-    {
-      userId: 15,
-      userName: '김15',
-      grade: '회원',
-    },
-    {
-      userId: 16,
-      userName: '김16',
-      grade: '회원',
-    },
-    {
-      userId: 17,
-      userName: '오17',
-      grade: '용병',
-    },
-    {
-      userId: 18,
-      userName: '김18',
-      grade: '용병',
-    },
-  ],
-};
+import api from '@/api/core';
 
 interface MemberElementType {
   userId: number;
@@ -105,102 +10,62 @@ interface MemberElementType {
   grade: string;
 }
 
-const { playerManange, categoryTitle, playerDetailInfo } = style;
+const { playerManange } = style;
 
+// 어떤 타입인지는 이름을 명시적으로 바꿔줘야할듯?
 const TeamPlayerManage = () => {
-  const { member } = dummyData;
-  const [memberInfo, setMemberInfo] = useState({});
-  const [hasAuthorized, setHasAuthorized] = useState(true);
-  const [isEnterEditPage, setisEnterEditPage] = useState(true);
+  const [peopleType, setPeopleType] = useState('');
+  const [isEnterEditPage, setIsEnterEditPage] = useState(true);
+  const [hasAuthorization, setHasAuthorization] = useState(true);
+  const [memberInfo, setMemberInfo] = useState<MemberElementType[]>([]);
+  const [teamId, setTeamId] = useState(window.location.pathname.split('/')[2]);
+  const isAddTeamMember = hasAuthorization && isEnterEditPage === false;
 
-  // TOOD: 컴포넌트 분리 시, 하위 컴포넌트로 이동 예정
-  const captain = member.find((player: MemberElementType) => player.grade === '주장');
-  const subCaptain = member.find((player: MemberElementType) => player.grade === '부주장');
-  const normalMember = member.filter((player: MemberElementType) => player.grade === '회원');
-  const mercenary = member.filter((player: MemberElementType) => player.grade === '용병');
-
-  const handleEditTeamPlayerInfo = () => {
-    setisEnterEditPage(true);
+  const handleChangeEditButtonStatus = () => {
+    setIsEnterEditPage(!isEnterEditPage);
   };
 
-  // TODO: useEffect를 통해 데이터를 get 해올 예정
   useEffect(() => {
-    // 최초에 화면 진입 시, 권한을 확인하는 로직
+    const newTeamId = window.location.pathname.split('/')[2];
+    const newPeopleType = window.location.pathname.split('/')[3];
+
+    if (newTeamId) {
+      setTeamId(newTeamId);
+    }
+
+    if (newPeopleType) {
+      setPeopleType(newPeopleType);
+    }
+
+    // TODO: 로그인 페이지 머지된 이후에, 리덕스에서 정보를 받아올 예정.
     // if (userGrade[`${teamName}`] === '팀장') {
-    //   setHasAuthorized(true);
+    //   setHasAuthorization(true);
     // }
-    // 팀원 정보를 받아오면, 그 값을 state로 넘겨준다.
-  }, []);
+
+    // TODO: API 연결 시, 주석 제거 예정
+    // const getPeopleInfo = async () => {
+    //   const { data } = await api.get({
+    //     url: `/teams/${teamId}/${peopleType}`,
+    //   });
+
+    //   setMemberInfo(data);
+    // };
+
+    // getPeopleInfo();
+  }, [teamId, peopleType]);
 
   return (
     <>
       <Header />
       <div className={classNames(playerManange)}>
-        <article className={classNames(playerDetailInfo)}>
-          {/* article Header */}
-          <div>
-            <p className={classNames(categoryTitle)}>팀원 정보</p>
-            {hasAuthorized &&
-              (isEnterEditPage ? (
-                <button type="button" onClick={handleEditTeamPlayerInfo}>
-                  완료
-                </button>
-              ) : (
-                <button type="button" onClick={handleEditTeamPlayerInfo}>
-                  수정하기
-                </button>
-              ))}
-          </div>
-          {/* team player || 용병 데이터 */}
-          <ul>
-            {isEnterEditPage ? (
-              <>
-                <li>
-                  {captain?.userName} {captain?.grade}
-                </li>
-                <li>
-                  {subCaptain?.userName}
-                  <select>
-                    <option>{subCaptain?.grade}</option>
-                    <option>회원</option>
-                  </select>
-                </li>
-                {normalMember.map((player: MemberElementType) => (
-                  <li>
-                    {player?.userName}
-                    <select>
-                      <option>{player?.grade}</option>
-                      <option>부팀장</option>
-                    </select>
-                  </li>
-                ))}
-              </>
-            ) : (
-              <>
-                <li>
-                  {captain?.userName} {captain?.grade}
-                </li>
-                <li>
-                  {subCaptain?.userName} {subCaptain?.grade}
-                </li>
-                {normalMember.map((player: MemberElementType) => (
-                  <li>
-                    {player?.userName}
-                    {player?.grade}
-                  </li>
-                ))}
-              </>
-            )}
-          </ul>
-        </article>
-        {/* button div */}
-        {isEnterEditPage && (
-          <div>
-            {/* TODO:  onClick도 상위에서 내려주는 방식으로 추가 예정 */}
-            <button type="button">방출</button>
-            <button type="button">위임</button>
-          </div>
-        )}
+        <PeopleList // TODO: PeopleList 네이밍을 명시적으로 변경 예정
+          isEditing={isEnterEditPage}
+          isMember={peopleType === 'member'}
+          peopleInfo={memberInfo}
+          hasAuthorization={hasAuthorization}
+          handleChangeEditButtonStatus={handleChangeEditButtonStatus}
+        />
+        {isAddTeamMember && <button type="button">팀원 초대</button>}
       </div>
     </>
   );
