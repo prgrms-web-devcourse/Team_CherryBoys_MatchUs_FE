6500a1f740517bd22d7fc21835fbfda273c5b37e
Yongrok <94chl@naver.com>
2021-12-10 01:03:46 +0900

    feat: 매칭 신청, 수락, 평가 컴포넌트 내보내기 추가

--- src/components/Match/index.ts
+++ src/components/Match/index.ts
@@ -2,3 +2,6 @@ export { default as MatchDetail } from './MatchDetail/MatchDetail';
 export { default as MatchInfo } from './MatchInfo/MatchInfo';
 export { default as MatchButton } from './MatchButton/MatchButton';
 export { default as TeamCard } from './TeamCard/TeamCard';
+export { default as MatchApplyModal } from './MatchApplyModal/MatchApplyModal';
+export { default as MatchApproveModal } from './MatchApproveModal/MatchApproveModal';
+export { default as MatchReviewModal } from './MatchReviewModal/MatchReviewModal';
