1ed2905afa4cfa231f46f734b6f5810756bab17a
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-12-12 12:04:04 +0900

    feat: MatchListElement Component 구현

---
+++ src/components/MatchDetail/MatchListElement/MatchElement.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface MatchInfoProps {
+  matchId: number;
+  matchDate: string;
+  registerTeamName: string;
+  registerTeamLogo: string;
+  applyTeamName: string;
+  applyTeamLogo: string;
+  status: string;
+}
+
+const MatchListElement = ({
+  matchId,
+  matchDate,
+  registerTeamLogo,
+  registerTeamName,
+  applyTeamLogo,
+  applyTeamName,
+  status,
+}: MatchInfoProps) => {
+  return (
+    <>
+      <div>
+        <img src={registerTeamLogo} alt="등록 팀 로고 이미지" />
+        <p>{registerTeamName}</p>
+      </div>
+      <span>vs</span>
+      <div>
+        <img src={applyTeamLogo} alt="등록 팀 로고 이미지" />
+        <p>{applyTeamName}</p>
+      </div>
+    </>
+  );
+};
+
+export default MatchListElement;
