d3d7b7da6e6773677c1ea42870659617b0f29b3f
Yongrok <94chl@naver.com>
2021-12-13 16:25:52 +0900

    refactor: input 컴포넌트 기능 수정

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -5,3 +5,4 @@ export { default as Posts } from './Posts/Posts';
 export { default as PostItem } from './PostItem/PostItem';
 export { default as Header } from './Header/Header';
 export { default as TabBar } from './TabBar/TabBar';
+export * from './Inputs';
