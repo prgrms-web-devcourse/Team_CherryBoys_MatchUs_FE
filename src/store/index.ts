e507346247940d04391421cedb681c5e609d664f
Yongrok <94chl@naver.com>
2021-12-13 15:37:05 +0900

    feat: Match에 필요한 Match 스토어 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { match } from './match/match';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    match: match.reducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
