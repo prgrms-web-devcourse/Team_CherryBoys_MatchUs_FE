5bb1eb9d333c952162399bf7acb7e465786c19af
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-12-14 13:57:12 +0900

    feat: 스크린 리더에 읽히는 페이지 정보 추가

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -11,6 +11,7 @@ const NotFound = () => {
 
   return (
     <div className={classNames(componentContainer)}>
+      <h1 className={classNames('s_a11yHidden')}>Not Found 페이지</h1>
       <div>
         <img src={errorIcon} alt="에러 아이콘" />
       </div>
