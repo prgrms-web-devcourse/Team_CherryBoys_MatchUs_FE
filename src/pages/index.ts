56b0159e07524cbb8936322dfdd42c7899b87930
khj <hongjung.kim.dev@gmail.com>
2021-12-15 00:08:59 +0900

    refactor: merge과정에서 실수로 경로 잘못 적은것 수정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export { default as Matches } from './Matches/Matches';
+export { default as Matches } from './Matches/MatchesPage';
 export { default as Hires } from './Hires/Hires';
 export { default as Teams } from './Teams/Teams';
 export { default as Users } from './Users/Users';
