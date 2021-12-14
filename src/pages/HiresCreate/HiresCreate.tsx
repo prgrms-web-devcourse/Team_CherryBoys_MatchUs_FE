5f5574c3e54cc9faf798401338a42a5600da1b64
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2021-12-14 17:03:59 +0900

    feat: route path를 적용하여 컴포넌트 라우팅 (#27)

---
+++ src/pages/HiresCreate/HiresCreate.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HiresCreate = () => {
+  return (
+    <div>
+      <h1>Hires create</h1>
+    </div>
+  );
+};
+
+export default HiresCreate;
---
+++ src/pages/HiresEdit/HiresEdit.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HiresEdit = () => {
+  return (
+    <div>
+      <h1>Hires edit</h1>
+    </div>
+  );
+};
+
+export default HiresEdit;
