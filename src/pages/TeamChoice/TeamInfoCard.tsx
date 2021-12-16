f17f077022c0236c198631da6ccafffff34fb340
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-12-16 21:32:55 +0900

    feat: 팀 정보를 보여주는 TeamInfoCard 컴포넌트 생성

---
+++ src/pages/TeamChoice/TeamInfoCard.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import classNames from 'classnames';
+import style from './teamChoice.module.scss';
+
+export interface TeamCardInfo {
+  mannerTemperature: number;
+
+  tagNames: string[];
+  teamCreatedAt: string;
+  teamId: number;
+  teamName: string;
+}
+
+const { cardContainer } = style;
+
+const TeamInfoCard = ({
+  teamId,
+  teamName,
+  teamCreatedAt,
+  tagNames,
+  mannerTemperature,
+}: TeamCardInfo) => {
+  return (
+    <div className={classNames(cardContainer)}>
+      <div key={`teamCard-${teamId}`}>
+        {/* TODO: API를 통해 로고가 넘어오면, 채울 예정 */}
+        <div>{/* <img src={} /> */}</div>
+      </div>
+      <div>
+        <p>
+          <span>{teamName}</span>
+          <span>{teamCreatedAt}</span>
+        </p>
+        <div>
+          <article>
+            {tagNames.map((tag) => {
+              return <span>{tag}</span>;
+            })}
+          </article>
+          <div>{mannerTemperature}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TeamInfoCard;
