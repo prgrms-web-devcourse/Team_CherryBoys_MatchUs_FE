cb417870e85e8a448c5bc5f85deb6f4a064e9621
progwon <dhwhc0711@naver.com>
2021-12-16 21:24:45 +0900

    feat: 타입 명시위한 useAppDispatch 추가.
    타입 명시를 위한 useAppDispatch 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 import authReducer from './authSlice';
 import { match } from './match/match';
 import { posts } from './posts';
@@ -7,9 +8,10 @@ export const store = configureStore({
   reducer: {
     posts: posts.reducer,
     match: match.reducer,
-    auth: authReducer,                    
+    auth: authReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
