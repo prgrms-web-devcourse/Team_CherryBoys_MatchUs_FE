734fc98ac83d43829b171cc052a9722279915c6e
DongHyeon Kim <49136186+Kimbangg@users.noreply.github.com>
2021-12-17 14:40:38 +0900

    feat: 유저 매치 상세페이지 추가 (MAT-218) (#41)
    * feat: 유저 매치 상세페이지 추가
    
    * refactor: 파라미터를 받을 때, 구조분해할당을 하는 방식으로 변경

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,14 +1,16 @@
 import { throwErrorMessage } from '@/utils/api';
 import api from '@/api/core';
 
+export const getUserMatchHistory = (userId: number | undefined) =>
+  api
+    .get({
+      url: `users/${userId}/matches`,
+    })
+    .catch(throwErrorMessage);
+
 export const getUserInfo = (userId: number | undefined) =>
   api
     .get({
       url: `/users/${userId}`,
     })
     .catch(throwErrorMessage);
-
-export const getUserMatchHistory = (userId: number | undefined) =>
-  api.get({
-    url: `users/${userId}/matches`,
-  });
