916da322a2f172003672e4b57149555a733163de
progwon <dhwhc0711@naver.com>
2021-12-18 17:07:07 +0900

    feat: login 페이지 css 적용
    login 페이지에 css 적용

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -8,6 +8,7 @@ import { login } from '@/store/userSlice';
 import { RootState, useAppDispatch } from '@/store';
 import { CustomLabel, CustomInput, CustomModalDialog } from '@/components';
 import style from './login.module.scss';
+import logo from '@/assets/images/logo.png';
 
 const Login = () => {
   const isLogged = useSelector((state: RootState) => state.user.isLogged);
@@ -17,7 +18,16 @@ const Login = () => {
     password: '',
   });
   const { email, password } = loginForm;
-  const { modalSubTitle, modalMainTitle } = style;
+  const {
+    modalSubTitle,
+    modalMainTitle,
+    container,
+    form__label,
+    form__login,
+    signup,
+    form__input,
+    logo_img,
+  } = style;
   const dispatch = useAppDispatch();
   const history = useHistory();
 
@@ -44,7 +54,9 @@ const Login = () => {
   };
 
   return (
-    <>
+    <div className={classNames(container)}>
+      <img src={logo} alt="로고" className={classNames('whiteSpcae', logo_img)} />
+      <h1 className={classNames('a11yHidden')}>회원 가입 페이지</h1>
       {isModalDialogOpen && (
         <CustomModalDialog
           buttonLabel="확인"
@@ -59,23 +71,50 @@ const Login = () => {
           </span>
         </CustomModalDialog>
       )}
-      <form onSubmit={handleSubmit}>
-        <CustomLabel htmlFor="email">아이디</CustomLabel>
-        <CustomInput type="text" name="email" id="email" value={email} onChange={handleChange} />
-        <CustomLabel htmlFor="password">패스워드</CustomLabel>
-        <CustomInput
-          name="password"
-          id="password"
-          value={password}
-          type="password"
-          onChange={handleChange}
-        />
-        <button type="submit">로그인</button>
-      </form>
-      <button type="button" onClick={() => history.push('signup')}>
+      <div>
+        <img src={logo} alt="로고" className={classNames('whiteSpcae', logo_img)} />
+      </div>
+      {/* 로그인 폼 */}
+      <div>
+        <form onSubmit={handleSubmit}>
+          <CustomLabel htmlFor="email" className={classNames('whiteSpcae', form__label)}>
+            아이디
+          </CustomLabel>
+          <CustomInput
+            type="text"
+            name="email"
+            id="email"
+            placeholder="아이디를 입력해주세요."
+            value={email}
+            onChange={handleChange}
+            className={classNames('whiteSpace', form__input)}
+          />
+          <CustomLabel htmlFor="password" className={classNames('whiteSpcae', form__label)}>
+            패스워드
+          </CustomLabel>
+          <CustomInput
+            type="password"
+            name="password"
+            id="password"
+            placeholder="패스워드를 입력해주세요."
+            value={password}
+            onChange={handleChange}
+            className={classNames('whiteSpace', form__input)}
+          />
+
+          <button type="submit" className={classNames('whiteSpace', form__login)}>
+            로그인
+          </button>
+        </form>
+      </div>
+      <button
+        type="button"
+        onClick={() => history.push('signup')}
+        className={classNames('whiteSpace', signup)}
+      >
         회원가입
       </button>
-    </>
+    </div>
   );
 };
 
