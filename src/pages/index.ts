e85a33948d482b4ddae5185cc41b80521b4075f6
progwon <dhwhc0711@naver.com>
2021-12-19 19:12:21 +0900

    feat: index.ts에 페이지 import
    index.ts에 페이지 import

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -24,3 +24,6 @@ export { default as Main } from './Main/Main';
 
 export { default as UserDetail } from './UserDetail/UserDetail';
 export { default as UserMatchDetail } from './UserMatchDetail/UserMatchDetail';
+export { default as UserHireRequest } from './UserHireRequest/UserHireRequest';
+export { default as UserInfoEdit } from './UserInfoEdit/UserInfoEdit';
+export { default as UserTeamInvitation } from './UserTeamInvitation/UserTeamInvitation';
