5ef953159e2d3248d6fbd0dbe4c13f69f71306f4
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-12-21 02:49:51 +0900

    fix: 불필요한 useEffect Call 제거

--- src/pages/Hires/Hires.tsx
+++ src/pages/Hires/Hires.tsx
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchAllPost, HiresResponseType } from '@/store/posts';
+import { HiresResponseType } from '@/store/posts';
 import { Posts } from '@/components';
 import { getHiresInfo } from '@/api/hires';
 // Todo(홍중) : 임시 데이터, 팀 정보 관리하는곳에서 가져오기(12/13)
 const selectedTeam = { teamId: 3, grade: 'CAPTAIN' };
 
 const Hires = () => {
-  const dispatch = useDispatch();
   const [data, setData] = useState<Array<HiresResponseType>>([]);
-
-  useEffect(() => {
-    dispatch(fetchAllPost());
-  }, [dispatch]);
 
   useEffect(() => {
     const getCurrentHiresInfo = async () => {
