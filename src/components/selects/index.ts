d8059a8fe9ffff2068d51920d4995f7ae7b8e3eb
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2021-12-20 16:58:48 +0900

    feat: 용병 생성 페이지에서 사용자 입력에 따라 서버에 요청, 완료후 용병 리스트 페이지로 이동 (MAT-262) (#74)
    * feat: 용병 등록 페이지에서 장소를 더미데이터에서 서버에서 받아온 정보로 변경
    
    * feat: 용병 페이지 팀선택 추가, 생성 버튼 누를시 서버에 보내는 데이터 수정
    
    * feat: 용병 생성 페이지에서 사용자 입력에 따라 서버에 요청, 완료후 용병 리스트 페이지로 이동
    
    * refactor: 절대경로로 수정

--- src/components/selects/index.ts
+++ src/components/selects/index.ts
@@ -2,3 +2,4 @@ export { default as AgeGroup } from './AgeGroup';
 export { default as Date } from './Date';
 export { default as Place } from './Place';
 export { default as HiresPosition } from './HiresPosition';
+export { default as LocationSelect } from './LocationSelect';
