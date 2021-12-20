8b4091fc77406d566218893bf389cd0798ba1681
Yongrok <94chl@naver.com>
2021-12-20 18:58:57 +0900

    refactor: 용병 구인글 생성, 수정 페이지 css 수정

--- src/components/selects/AgeGroup.tsx
+++ src/components/selects/AgeGroup.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
+import classNames from 'classnames';
 import { AGE_GROUP } from '@/consts';
+import style from './select.module.scss';
+
+const { inputAgeGroupBox } = style;
 
 interface Props {
   ageGroup?: number;
@@ -8,16 +12,18 @@ interface Props {
 }
 const AgeGroup = ({ ageGroup, handleChangeAge }: Props) => {
   return (
-    <div>
-      <div>연령대</div>
-      <select id="hiresAgeGroup" onChange={handleChangeAge}>
-        <option>{`${ageGroup || AGE_GROUP[0].slice(0, 2)}대`}</option>
-        {AGE_GROUP.map((group) => (
-          <option id={`${group}대`} key={`age-${group}`}>
-            {group}
-          </option>
-        ))}
-      </select>
+    <div className={classNames(inputAgeGroupBox)}>
+      <h3>연령대</h3>
+      <div>
+        <select id="hiresAgeGroup" onChange={handleChangeAge}>
+          <option>{`${ageGroup || AGE_GROUP[0].slice(0, 2)}대`}</option>
+          {AGE_GROUP.map((group) => (
+            <option id={`${group}대`} key={`age-${group}`}>
+              {group}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 };
