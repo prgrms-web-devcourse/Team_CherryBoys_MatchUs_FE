10446577cd3655d203a8636518bc6ecee1485b79
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-02-27 21:49:53 +0900

    refactor: AppLike한 웹뷰로 변경

--- src/components/DefaultTemplate/DefaultTemplate.tsx
+++ src/components/DefaultTemplate/DefaultTemplate.tsx
@@ -8,10 +8,10 @@ interface Props {
 }
 
 const DefaultTemplate = ({ children }: Props) => {
-  const { main } = styles;
+  const { main, mainContainer } = styles;
 
   return (
-    <>
+    <div className={classNames(mainContainer)}>
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
@@ -19,7 +19,7 @@ const DefaultTemplate = ({ children }: Props) => {
       <Header />
       <main className={classNames(main)}>{children}</main>
       <TabBar />
-    </>
+    </div>
   );
 };
 
